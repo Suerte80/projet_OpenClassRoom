8e559d59b1f848fb1fa24ef575c6673ef9f5b22c
Alexis Courtin <alexiscourtin3@gmail.com>
2015-04-02 21:26:33 +0200

    Ajout message dans main.c

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 
 int main( int argc, char **argv, char **penv )
 {
+  printf("Mon beau projet :/");
+
   for( int i = 0; penv[i] != NULL; i++ )
-    printf("%s\n", penv[i]);
+    printf("%s\n", penv[i] );
 }
